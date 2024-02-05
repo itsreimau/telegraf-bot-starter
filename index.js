8c7023b92a0b265bc34429d75c55c763a6c3f68d
Muhamad Ikbal Maulana | ItsReimau <private.iblmln@gmail.com>
2024-02-05 23:14:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import {
     Telegraf
 } from "telegraf";
 
-const bot = new Telegraf(token);
+const bot = new Telegraf(process.env.BOT_TOKEN);
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Set the bot API endpoint
 app.use(await bot.createWebhook({
-    domain: webhookDomain
+    domain: process.env.WEBHOOK_DOMAIN
 }));
 
 bot.on("text", ctx => ctx.reply("Hello"));
