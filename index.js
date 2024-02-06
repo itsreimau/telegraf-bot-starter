e6d99e4c845b29a111e30abb3394e0bdaa69cc25
Muhamad Ikbal Maulana | ItsReimau <private.iblmln@gmail.com>
2024-02-06 16:59:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
-const express = require('express');
-const {
+import express from "express";
+import {
     Telegraf
-} = require('telegraf');
-/* const {
-    TelegrafCommandHandler
-} = require('telegraf-command-handler-upgraded'); */
-const path = require('path');
+} from "telegraf";
 
-// Inisialisasi bot Telegram
+// Initialize Telegram bot
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// Inisialisasi aplikasi Express
+// Initialize the Express application
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Tentukan fungsi async untuk menyiapkan webhook
-const setupWebhook = async () => {
+// Use middleware to parse incoming JSON requests
+app.use(express.json());
+
+// Set the bot API endpoint
+app.use(
     await bot.createWebhook({
         domain: process.env.WEBHOOK_DOMAIN,
-    });
-};
+    })
+);
 
-// Gunakan fungsi async untuk menyiapkan webhook
-app.use(async (req, res, next) => {
-    await setupWebhook();
-    next();
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something went wrong!");
 });
 
-// Menangani perintah
-/*
-const CommandHandler = new TelegrafCommandHandler({
-    path: path.resolve() + "/commands",
-});
-bot.use(CommandHandler.load());
-*/
+// Handle text messages from users
+bot.on("text", (ctx) => ctx.reply("Active!"));
 
-// Menangani pesan teks dari pengguna
-bot.on('text', (ctx) => ctx.reply('Apa?'));
-
-// Menjalankan server Express pada port yang telah ditentukan
-app.listen(port, () => console.log('Listening on port', port));
+// Runs an Express server on a specified port
+app.listen(port, () => console.log("Listening on port", port));
