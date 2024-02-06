1fecf895b399ab3a2843a3663522845bf27a7225
Muhamad Ikbal Maulana | ItsReimau <private.iblmln@gmail.com>
2024-02-07 00:01:56 +0700

    update commands/start.js

--- commands/start.js
+++ commands/start.js
@@ -1,6 +1,6 @@
 export default {
-    name: 'start',
-    async execute: (ctx) => {
+    name: 'example',
+    async execute(ctx) {
         ctx.reply('This is an example of a command!');
     }
 };
