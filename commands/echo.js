c738d18379b9d661bcdb906748ac2ed03298205b
itsreimau <private.iblmln@gmail.com>
2024-04-03 15:09:01 +0700

    small changes

--- commands/echo.js
+++ commands/echo.js
@@ -3,10 +3,13 @@ export default {
     async execute(ctx) {
         const args = ctx.message.text.split(' ').slice(1).join(' ');
 
-        if (args) {
+        if (!args) return ctx.reply('Give an argument!');
+
+        try {
             ctx.reply(args);
-        } else {
-            ctx.reply('Give an argument!');
+        } catch (error) {
+            console.error('Error:', error);
+            return ctx.reply(`Error: ${error.message}`);
         }
     }
 };
