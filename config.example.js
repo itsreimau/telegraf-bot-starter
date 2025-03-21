5638398198ea7305dab9723ee560bb11b25e0e41
itsreimau <private.iblmln@gmail.com>
2025-03-21 20:04:32 +0700

    chore: Update version to 1.1.4

--- config.example.js
+++ config.example.js
@@ -2,11 +2,11 @@
 global.config = {
     // Bot Configuration
     bot: {
-        token: "", // Token for accessing the Telegram Bot API
+        token: "" // Token for accessing the Telegram Bot API
     },
 
     // Developer Information
     developer: {
-        id: "", // Developer ID for identifying who has special access
+        id: "" // Developer ID for identifying who has special access
     }
 }
